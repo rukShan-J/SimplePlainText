6495ddb54a5df60f364658d05dfe872c4962f25c
Pubudu Rukshan <jayananda.apr@gmail.com>
2025-05-02 23:28:17 +0530

    Created a basic program to print hello world via a servlet

---
+++ src/java/PlainTextServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class PlainTextServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        PrintWriter out = response.getWriter();
+        out.println("Hello World!");
+    }
+
+}
